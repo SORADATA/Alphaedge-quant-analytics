dc9187f249b2906a8655355a795b03c86cc5521c
sora <moussa.sissoko6@etu.univ-lorraine.fr>
2026-09-24 12:14:44 +0000

    fix: resolve indentation problem in _add_momentum_factors

--- src/features/alpha_features.py
+++ src/features/alpha_features.py
@@ -35,7 +35,7 @@ def _add_momentum_factors(df: pd.DataFrame, g) -> pd.DataFrame:
     for lag in lags:
         df[f"return_{lag}m"] = g["adj close"].transform(
             lambda x, lag=lag: x.pct_change(lag)
-            )
+        )
     pct_12 = g["adj close"].transform(lambda x: x.pct_change(12))
     pct_1 = g["adj close"].transform(lambda x: x.pct_change(1))
     pct_6 = g["adj close"].transform(lambda x: x.pct_change(6))
